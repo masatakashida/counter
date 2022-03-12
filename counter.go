efe8039bfa7cb6a2fe9b15304ffcf06f6d8e112a
Masataka Shida <ar.tec.masa@gmail.com>
2022-03-12 18:30:20 +0900

    add counter program

--- counter.go
+++ counter.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func displayCount(num uint8) {
+	fmt.Printf("count: %d\n", num)
+	fmt.Print("(increment => i, clear => c, exit => e) >")
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Print("(increment => i, clear => c, exit => e) >")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var count uint8 = 0
+
+	for {
+		scanner.Scan()
+		input := scanner.Text()
+
+		switch input {
+		case "", "i", "I":
+			count++
+			fmt.Println("increment counter")
+			displayCount(count)
+			continue
+		case "c", "C":
+			count = 0
+			fmt.Println("clear counter")
+			displayCount(count)
+			continue
+		case "e", "E":
+			fmt.Println("exit counter")
+			goto L
+		default:
+			fmt.Println("try again")
+			displayCount(count)
+			continue
+		}
+	}
+L:
 }
