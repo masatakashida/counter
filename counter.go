7f023497db62fef3f6b03dff7aa3dcaf2fbbf09d
Masataka Shida <ar.tec.masa@gmail.com>
2023-03-20 23:24:14 +0900

    change timing of counter display

--- counter.go
+++ counter.go
@@ -6,16 +6,13 @@ import (
 	"os"
 )
 
-func displayCount(num uint8) {
-	fmt.Printf("count: %d\n", num)
-}
-
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	var count uint8 = 0
 
 	Loop: for {
+		fmt.Printf("count: %d\n", count)
 		fmt.Print("Enter a value (type 'exit' to quit): ")
 		scanner.Scan()
 		input := scanner.Text()
@@ -24,7 +21,6 @@ func main() {
 		case "increment", "", "i", "I":
 			count++
 			fmt.Println("increment count")
-			displayCount(count)
 			continue
 		case "decrement", "d", "D":
 			if count > 0 {
@@ -33,19 +29,16 @@ func main() {
 			} else {
 				fmt.Println("count is less than or equal to 0")
 			}
-			displayCount(count)
 			continue
 		case "clear" ,"c", "C":
 			count = 0
 			fmt.Println("clear count")
-			displayCount(count)
 			continue
 		case "exit", "e", "E":
 			fmt.Println("exit counter")
 			break Loop
 		default:
 			fmt.Println("counter: command not found")
-			displayCount(count)
 			continue
 		}
 	}
